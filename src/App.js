f6cd4b2bfd9cfa60280ef06d5a2a8fdbb975484d
slicedearth <47583576+slicedearth@users.noreply.github.com>
2020-02-20 15:04:05 +1100

    JSX Revision

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React, { Component } from 'react';
+// eslint-disable-next-line
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
 class App extends Component {
+  fullName = () => 'Mary Smith';
   render() {
+    const name = 'John';
+    const city = () => 'Melbourne';
     return (
-      <div>
-        <h1>Greetings</h1>
+      // Vanilla JS
+      // React.createElement(
+      //   'div',
+      //   { className: 'App' },
+      //   React.createElement('h1', null, 'Greetings')
+      // )
+
+      // JSX
+      <div className='App'>
+        <h1>
+          Greetings From {name.toUpperCase()} Version {134.1 + 0.8}
+        </h1>
+        <h1>
+          {name.toLowerCase()} is from {city()}
+        </h1>
+        <h1>{this.fullName()}</h1>
+        <label htmlFor=''></label>
       </div>
+      // <>
+      // <h1>Greetings</h1>
+      // </>
     );
   }
 }
