78f08f1d0fdd88464f9d2f44110c6c452622134d
slicedearth <47583576+slicedearth@users.noreply.github.com>
2020-03-04 10:18:11 +1100

    Created Search Component
    Created Search.js and made changes to the GitHub API connection.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios';
+import Search from './components/users/Search';
 class App extends Component {
   state = {
     users: [],
@@ -11,7 +12,9 @@ class App extends Component {
   };
   async componentDidMount() {
     this.setState({ loading: true });
-    const res = await axios.get('https://api.github.com/users');
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
     this.setState({ users: res.data, loading: false });
     console.log(res.data);
   }
@@ -21,6 +24,7 @@ class App extends Component {
         {/* <Navbar title='GitHub Finder' icon='fab fa-github' /> */}
         <Navbar />
         <div className='container'>
+          <Search />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
